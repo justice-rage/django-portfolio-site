e3ca938452b63e9a7a4667cc00d5007c28ab5d81
Justice Peltier <peltierjustice@gmail.com>
2020-11-06 10:10:35 -0500

    Setting up models and views
    Created About, Project, and Contact sections in models and views.

--- jpd_website/homepage/models.py
+++ jpd_website/homepage/models.py
@@ -5,13 +5,13 @@ from django.db import models
 # About model is the "About Me" section of my website.
 class About(models.Model):
     title = models.CharField(max_length=100)
-    introduction = models.TextField()
+    content = models.TextField()
     image = models.CharField(max_length=100)
     
     # returns name/title of object instead of 'object (#)'
     def __str__(self):
         return self.title
-
+"""------------------------------------------------------------------------"""
 # Project model is the "Projects" section of my website.
 class Project(models.Model):
     title = models.CharField(max_length=100)
@@ -22,3 +22,10 @@ class Project(models.Model):
     # returns name/title of object instead of 'object (#)'
     def __str__(self):
         return self.title #or self.title for blog posts
+"""------------------------------------------------------------------------"""
+# Contact is the "Contact me" section of my website
+class Contact(models.Model):
+    name =
+    subject = 
+    email = 
+    message = 
--- jpd_website/homepage/views.py
+++ jpd_website/homepage/views.py
@@ -3,3 +3,20 @@ from homepage.models import About, Project
 
 # Create your views here.
 
+# About section
+def about_me(request):
+"""------------------------------------------------------------------------"""
+# Project section
+def all_projects(request):
+    # query database to return all project objects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html',
+                    {'projects': projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html',
+                    {'project': project})
+"""------------------------------------------------------------------------"""
+# Contact section
+def contact_me(request):
