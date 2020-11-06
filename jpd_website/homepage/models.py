2d14b312a7cc91c47668f7fddea5e5814c414f53
Justice Peltier <peltierjustice@gmail.com>
2020-11-06 09:53:05 -0500

    Created models and imported to views
    Models:
    1) About - About me section
    2) Project - Project section

--- jpd_website/homepage/models.py
+++ jpd_website/homepage/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+# About model is the "About Me" section of my website.
+class About(models.Model):
+    title = models.CharField(max_length=100)
+    introduction = models.TextField()
+    image = models.CharField(max_length=100)
+    
+    # returns name/title of object instead of 'object (#)'
+    def __str__(self):
+        return self.title
+
+# Project model is the "Projects" section of my website.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.CharField(max_length=100)
+    
+    # returns name/title of object instead of 'object (#)'
+    def __str__(self):
+        return self.title #or self.title for blog posts
--- jpd_website/homepage/views.py
+++ jpd_website/homepage/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from homepage.models import About, Project
 
 # Create your views here.
+
